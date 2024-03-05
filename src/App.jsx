b3202beeb2ed3fc7f3981a4f8670c99423fc12f7
Emin Suleymanov <121028869+suleymanovemin@users.noreply.github.com>
2024-03-05 21:26:02 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -112,6 +112,7 @@ function App({ dispatch, user }) {
   //       });
   //     });
   // }, []);
+  
   useEffect(() => {
     fetch(
       "https://raw.githubusercontent.com/suleymanovemin/finalProject/master/data.json"
